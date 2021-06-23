31c1614cc916d1aeceeaf8a7281b4d774fabb644
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-06-23 15:32:19 -0400

    finished basic elements

---
+++ tippo.v2/TipViewController.h
@@ -0,0 +1,16 @@
+//
+//  TipViewController.h
+//  tippo.v2
+//
+//  Created by Paula Leticia Geronimo on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TipViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ tippo.v2/TipViewController.m
@@ -0,0 +1,55 @@
+//
+//  TipViewController.m
+//  tippo.v2
+//
+//  Created by Paula Leticia Geronimo on 6/23/21.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *billFields;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentageControl;
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    
+    [self.view endEditing:true]; // why the [] ??
+    
+    
+}
+- (IBAction)updateLabels:(id)sender {
+    double tipPercentages[] = {0.15, 0.2, 0.25};
+    double tipPercentage = tipPercentages[self.tipPercentageControl.selectedSegmentIndex];
+    
+    double bill = [self.billFields.text
+                    doubleValue];
+    double tip = bill * tipPercentage;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
